31687d016569ba59e15a16de8cb15a747acdcebe
e28166 <e28166@FXH-0015840124>
2024-09-18 08:43:53 -0400

    sdfads

---
+++ src/lessons/Variables.java
@@ -0,0 +1,29 @@
+package lessons;
+
+public class Variables {
+
+	public static void main(String[] args) {
+
+		// double slash is a comment
+		// this is also a comment
+		
+		/* this is a comment
+		  this is continuation of the comment
+		 */
+		
+		// variables follow camelCase convention
+		
+		// variables
+		int myFirstVariable = 0;
+		double myDouble = 0.0;
+		char myChar = 'a';
+		String myString = "";
+		boolean myBoolean1 = true;
+		boolean myBoolean2 = false;
+		
+		
+		
+
+	}
+
+}
