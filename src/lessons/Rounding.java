5ccd6698f3c9a394ce970ae430ece62b5ccbc067
e28166 <e28166@FXH-0015840124>
2024-10-02 12:04:00 -0400

    s

--- src/lessons/Rounding.java
+++ src/lessons/Rounding.java
@@ -4,13 +4,11 @@ public class Rounding {
 
 	public static void main(String[] args) {
 		
-		double price = 13.678;
+		double price = 1337.8;
 		
-		price = price * 100;
-		price = Math.round(price);
-		price = price / 100;
 		
-		// output
+		price = Math.round(price);  // rounds to nearest integer
+		
 		System.out.println(price);
 	}
 
