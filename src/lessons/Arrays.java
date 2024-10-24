a430d9d09447b10a186d243ad06de1fe569baf37
e28166 <e28166@FXH-0015840124>
2024-10-24 11:59:40 -0400

    s

--- src/lessons/Arrays.java
+++ src/lessons/Arrays.java
@@ -8,10 +8,10 @@ public class Arrays {
 		// Uses indexes (starting at 0)
 		// Holds ELEMENTS of any SINGLE TYPE
 		// Arrays are NOT dynamic (can't grow or shrink after creation)
-		
-		// SYNTAX:
-		// dataType[] arrayIdentifier = new dataType[arraySize];
 
+		// SYNTAX:
+		// dataType[] arrayIdentifier = new datatype[arraySize];
+		
 		// EXAMPLES:
 		int[] myIntArray = new int[3];
 		double[] myDoubleArray = new double[5];
@@ -19,50 +19,47 @@ public class Arrays {
 		char[] myCharArray = new char[13];
 		String[] myStringArray = new String[17];
 		
-		// We can initialize arrays with data upon creation.
-		// EXAMPLE:
-		int[] myClassRoomNumbers = {140, 216, 2011, 1013};
+		// We can initialize arrays with data upon creation:
+		int[] myClassRoomNumbers = {140, 250, 222, 155};
+		
+		// Access room numbers:
 		System.out.println(myClassRoomNumbers[0]);
 		System.out.println(myClassRoomNumbers[1]);
 		System.out.println(myClassRoomNumbers[2]);
 		System.out.println(myClassRoomNumbers[3]);
 		
-		// EXAMPLE:
-
-		// variables
-		double payForYear = 0;
-		double[] myPayCheques = new double[5];
-
-		// input
-		myPayCheques[0] = 276.19;
-		myPayCheques[1] = 176.89;
-		myPayCheques[2] = 256.29;
-		myPayCheques[3] = 376.39;
-		myPayCheques[4] = 876.49;
+		// Paycheque example:
+		double[] myPayCheques = new double[6];
 		
-		// process (add all pays)
-//		payForYear = myPayCheques[0] + myPayCheques[1]
-//				+ myPayCheques[2] + myPayCheques[3] 
-//				+ myPayCheques[4];
+		// Add data to array:
+		myPayCheques[0] = 150.14;
+		myPayCheques[1] = 50.13;
+		myPayCheques[2] = 180.84;
+		myPayCheques[3] = 159.64;
+		myPayCheques[4] = 80.44;
+		myPayCheques[5] = 89.16;
 		
-		// let's add the pay using a for-loop instead
+		// add all pay for the year
+		double payForYear = 0.0;
+		
+		// payForYear = myPayCheques[0] + myPayCheques[1] + myPayCheques[2]
+		//		+ myPayCheques[3] + myPayCheques[4] + myPayCheques[5];
+				
+		// number of elements in array:
+		System.out.println("Number of pay cheques: " + myPayCheques.length);
+
+		// Use for-loop to add all paycheques
 		for(int i=0; i<myPayCheques.length; i++)  {
 			
 			payForYear = payForYear + myPayCheques[i];
 		}
-		
+			
 		System.out.println("Annual Pay: $" + payForYear);
 		
-		// to determine length of an array
-		System.out.println("Number of Paycheques: " + myPayCheques.length);
-		
-		
-		// Attempting to access elements outside of Array bounds will
-		// generate Exception error
-		
-		
-		
-		
+		// Attempting to access an element outside the length
+		// of array will generate an Exception Error:
+
+		//System.out.println(myPayCheques[6]);
 		
 	}
 
