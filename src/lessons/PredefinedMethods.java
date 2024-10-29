7dcf883b71fb1a1077edc755df1b62c768502789
e28166 <e28166@FXH-0015840124>
2024-10-29 09:09:48 -0400

    d

---
+++ src/lessons/PredefinedMethods.java
@@ -0,0 +1,32 @@
+package lessons;
+
+import java.util.Scanner;
+
+public class PredefinedMethods {
+
+	public static void main(String[] args) {
+		
+		// Predefined methods are code or commands written
+		// by Java developers, for use by Java users.
+		// These methods are used to within Java programs to
+		// solve simple or complex tasks.  The implementation
+		// of these methods are "encapsulated", such that we
+		// can not see them.
+		
+		// Java methods are contained inside "classes"
+		// Need to import "classes" to use methods.
+		// Exceptions are Math and String classes, because
+		// they're so common.
+		
+	
+		// When a method returns a value, must do something with it:
+		// 1. Print it
+		// 2. Assign to a variable
+		// 3. Use in a calculation
+		
+		
+		
+		
+	}
+
+}
