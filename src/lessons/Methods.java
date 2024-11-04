b5d20205d6989b2a59d58cdeebd90e95d6f2c9a9
e28166 <e28166@FXH-0015840124>
2024-11-04 12:31:48 -0500

    s

--- src/lessons/Methods.java
+++ src/lessons/Methods.java
@@ -4,18 +4,16 @@ public class Methods {
 	
 	public static void main(String[] args) {
 		
-		// System.out.println(Math.max(2, 7));
-		System.out.println(max(7, 7));
+		System.out.println(max(13, 13));
 		
-		String myCombinedString = "";
-		String string1 = "foo";
-		String string2 = "bar";
-		myCombinedString = combinStrings(string1, string2);
-		System.out.println(myCombinedString);
+		String myString = "";
+		String string1 = "Hello ";
+		String string2 = "World!";
+		myString = combineStrings(string1, string2);
+		System.out.println(myString);
 		
 		displayMenu();
 	}
-	
 	
 	// DESCRIPTION - Displays menu for program.
 	// PARAMETERS - none
@@ -23,30 +21,45 @@ public class Methods {
 	public static void displayMenu()  {
 		
 		System.out.println("Welcome to my program.");
-		System.out.println("Choice 1 ...");
-		System.out.println("Choice 2 ...");
+		System.out.println("Option 1...");
+		System.out.println("Option 2...");
+		System.out.println("Option 3...");
 	}
-	
 
+	
 	// DESCRIPTION - Returns the combination of two Strings.
 	// PARAMETERS - String a, String b
 	// RETURN TYPE - String
-	public static String combinStrings(String a, String b)  {
+	public static String combineStrings(String a, String b)  {
 		
-		return a + b;
+		return a+b;
 	}
+	
 	
 	
 	// DESCRIPTION - Returns the greater of two int values.
 	// PARAMETERS - int a, int b
 	// RETURN TYPE - int
 	public static int max(int a, int b)  {
-			
-		if (a > b)
+		
+		if (a>b)
 			return a;
-			
+		
 		else
 			return b;
-	}	
+	}
+	
+	// DESCRIPTION - Returns the greater of two double values.
+	// PARAMETERS - double a, double b
+	// RETURN TYPE - double
+	public static double max(double a, double b)  {
+			
+		if (a>b)
+			return a;
+		
+		else
+			return b;
+	}
+	
 	
 }
