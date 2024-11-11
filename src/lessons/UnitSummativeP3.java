f10c085784dbeb69493396e1e2d635bd24fc8776
e28166 <e28166@FXH-0015840124>
2024-11-11 13:01:04 -0500

    s

---
+++ src/lessons/UnitSummativeP3.java
@@ -0,0 +1,49 @@
+package lessons;
+import java.util.Scanner;
+
+public class UnitSummativeP3 {
+
+	// 	PROGRAM DESCRIPTION - program will ask user 6 best marks, calculate average
+	//						  then will determine if user meets minimum requirements
+	//						  for a given program
+	
+	
+	public static void main(String[] args) {
+
+		// variables
+		final int NUM_GRADES = 6; 
+		int[] grades = new int[NUM_GRADES];
+
+		// STEP 1 - get marks from user	
+//		grades = getMarks(NUM_GRADES);
+		
+		
+		
+		// STEP 2 - calculate average
+		
+		
+		
+		// STEP 3 - determine if user meets minimum requirements
+		
+		
+
+	}
+	
+	// DESCRIPTION - obtains grades from user
+	// PARAMETERS - int numGrades
+	// RETURN TYPE - int[]
+	public static int[] getMarks(int numGrades)   {
+		
+		// variables
+		Scanner sc = new Scanner(System.in);
+		int[] userMarks = new int[numGrades];
+		
+		
+	
+		
+		
+		
+		
+	}
+
+}
