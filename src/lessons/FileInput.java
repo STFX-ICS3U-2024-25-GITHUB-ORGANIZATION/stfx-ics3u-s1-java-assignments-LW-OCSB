32ae6d66b53462a69c690bc3202efc2ada889196
e28166 <e28166@FXH-0015840124>
2024-12-04 12:05:57 -0500

    s

--- src/lessons/FileInput.java
+++ src/lessons/FileInput.java
@@ -6,41 +6,40 @@ public class FileInput {
 
 	public static void main(String[] args) {
 
-		String[] product = new String[5];
+		// 3 parallel arrays
+		String[] products = new String[5];
 		int[] quantity = new int[5];
 		double[] price = new double[5];
 		
-		try  {
+		try {
 			
-			// can specify anywhere file is located, using full path
-			//FileReader file = new FileReader("C:/Users/e28166/Desktop/inventory.txt");
-			
-			// for this to work, file must be located in Java Project folder
+			// open the file
 			FileReader file = new FileReader("inventory.txt");
 			
 			// put contents of file into memory
 			BufferedReader buffer = new BufferedReader(file);
-		
-			// read contents line-by-line from buffer into arrays
-			for (int i=0; i<product.length; i++)  {
+			
+			for(int i=0; i<products.length; i++)  {
 				
-				product[i] = buffer.readLine();
-				quantity[i] = Integer.parseInt(buffer.readLine());
+				products[i] = buffer.readLine();
+				quantity[i] = Integer.parseInt(buffer.readLine());     
 				price[i] = Double.parseDouble(buffer.readLine());
 			}
 			
 			// clear memory
 			buffer.close();
+			
 		}
-
-		catch (Exception e)  {
+		
+		catch(Exception e)  {
 			
 			System.out.println("Error reading file.");
 		}
 		
-		for(int i=0; i<product.length; i++) {
+		// print contents of parallel arrays
+		for(int i=0; i<products.length; i++)  {
 			
-			System.out.println(quantity[i] + " " + product[i] + " $" + price[i]);
+			System.out.println(products[i] + " " + quantity[i] + " " + price[i]);
 		}
 	}
 }
--- src/lessons/ParallelArrays.java
+++ src/lessons/ParallelArrays.java
@@ -4,21 +4,18 @@ public class ParallelArrays {
 
 	public static void main(String[] args) {
 
-		// Create 2 arrays which are related
+		// create 3 arrays
+		String[] name = {"Amal", "Sadman", "Geetansh", "Ayaan"};
+		int[] money = {5, 12, 50, 200};
+		String[] reason = {"borrowed", "bribe", "randsom", "theft"};
 		
-		String[] names = {"Tanner", "Pyna", "Abid", "Jeremy", "Yuyen"};
-		int[] debt = {10, 14, 22, 16, 40};
-		String[] reason = {"ice cream", "black jack", "gas money", "hot pot", "more gas money"};
 		
-		
-		//System.out.println(names[2] + " owes me $" + debt[2] + " because of " + reason[2]);
 
-		for(int i=0; i< names.length; i++)  {
+		for(int i=0; i<name.length; i++)  {
 			
-			System.out.println(names[i] + " owes me $" + debt[i] + " because of " + reason[i]);
+			System.out.println(name[i] + " " + money[i] + " " + reason[i]);
 		}
 		
-
 	}
 
 }
