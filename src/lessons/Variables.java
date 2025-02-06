35750c8a7b85c34640a83bc46df021cb985e5e21
e28166 <e28166@FXH-0015840124>
2025-02-06 09:08:46 -0500

    hfdfdf

---
+++ src/lessons/Variables.java
@@ -0,0 +1,34 @@
+package lessons;
+
+public class Variables {
+
+	public static void main(String[] args) {
+
+		// two slashes denotes a comment
+		// comments are notes for programmers
+		// comments are not executed by Java
+		
+		/*  this is the beginning of a long comment
+		    which spans more than 1 line 
+		 */
+		
+		// common primitive data types
+		int myInteger = 0;
+		double myDouble = 0.0;
+		char myChar = 'a';
+		boolean myBoolean1 = true;
+		boolean myBoolean2 = false;
+		
+		// object data type
+		String myString = "Hello World!";
+		
+		// constant variable - not meant to be changed
+		final double INTEREST_RATE = 0.035;
+		
+		// Naming Convention - camelCase for variables
+							 - ALL_UPPER_CASE for constants
+		
+
+	}
+
+}
