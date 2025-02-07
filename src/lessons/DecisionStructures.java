d208863570a79d4fe0a6c04a7b5d315d7a9b83bc
e28166 <e28166@FXH-0015840124>
2025-02-07 09:13:06 -0500

    s

---
+++ src/lessons/DecisionStructures.java
@@ -0,0 +1,28 @@
+package lessons;
+
+public class DecisionStructures {
+
+	public static void main(String[] args) {
+
+		/*	Decision Operators:
+		 * 
+		 * 	> 	Greater than
+		 * 	< 	Less than
+		 * 	>=  Greater than or Equal to
+		 * 	<= 	Less than or Equal to
+		 * 	== 	Equal to
+		 * 	!=	Not Equal to
+		 */
+		
+		// variables
+		int myInteger = 13;
+		double myDouble = 7.0;
+		
+		if (myInteger > myDouble)  {
+			
+			System.out.println("Hello World!");
+		}
+
+	}
+
+}
---
+++ src/lessons/UserInput.java
@@ -0,0 +1,35 @@
+package lessons;
+
+import java.util.Scanner;
+
+public class UserInput {
+
+	public static void main(String[] args) {
+
+		// variables
+		Scanner sc = new Scanner(System.in);
+		int userInput = 0;
+		String userInput2 = "";
+		
+		// prompt user to enter a number
+		System.out.println("Please enter an integer: ");
+		
+		// use Scanner to get input and place into variable
+		userInput = sc.nextInt();
+		
+		// print contents of userInput
+		System.out.println("You entered: " + userInput);
+
+		// prompt user to enter a phrase
+		System.out.println("Please enter a phrase: ");
+		
+		// use Scanner to get input and place into variable
+		userInput2 = sc.nextLine();
+		
+		// print contents of userInput2
+		System.out.println("You entered: " + userInput2);
+		
+		sc.
+	}
+
+}
