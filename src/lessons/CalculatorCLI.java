e9aa37145d3687092c69c88a799ed32d7340cbf0
e28166 <e28166@FXH-0015840124>
2025-02-21 08:33:54 -0500

    s

---
+++ src/lessons/CalculatorCLI.java
@@ -0,0 +1,33 @@
+package lessons;
+import java.util.Scanner;
+
+public class CalculatorCLI {
+
+	public static void main(String[] args) {
+	
+		// variables
+		Scanner sc = new Scanner(System.in);
+		int num1, num2, sum;
+		
+		System.out.println("Add Calculator.");
+		System.out.println("\nEnter first number: ");
+
+		try {
+			
+			num1 = sc.nextInt();
+		}
+		
+		catch (Exception e)  {
+			
+			System.out.println("Invalid entry.  Exiting.");
+			System.exit(0);
+		}
+		
+		System.out.println("This should not print if we exit.");
+		
+		
+
+
+	}
+
+}
