42587a203c6f34c2991284084adc8c16a90e1f57
e28166 <e28166@FXH-0015840124>
2025-03-04 09:17:39 -0500

    s

--- src/lessons/RandomNumbers.java
+++ src/lessons/RandomNumbers.java
@@ -6,22 +6,22 @@ public class RandomNumbers {
 
 		int n = 2;
 		
-		// Math.random() generates double > 0 && < 1
-		//System.out.println(Math.random());
+		// Math.random() generates double between 0.0 and less than 21.0
+		System.out.println(Math.random());
 		
-		// To generate a random number > 0, multiply by n
-		//System.out.println(Math.random() * n);
+		// To generate a random number >= 0, multiply by n
+		System.out.println(Math.random() * n);
 	
 		// EXAMPLE 1
 		// casting double answer to int will truncate decimal
 		// This generates random number 0 to n-1
-		//System.out.println(  (int) ( Math.random() * n ) );
+		System.out.println(  (int) ( Math.random() * n ) );
 
 		// EXAMPLE 2
 		// Adding 1 to n, will increase the upper-limit by 1,
 		// but does not change the lower limit of 0
 		// This generates random number 0 to n
-		//System.out.println(  (int) ( Math.random() * (n+1) ) );
+		System.out.println(  (int) ( Math.random() * (n+1) ) );
 		
 		// EXAMPLE 3
 		// Adding 1 to EXAMPLE 1 will generate a random number
