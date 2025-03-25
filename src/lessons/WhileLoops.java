dc22bdde8d3ea8537ddb2f166efe84c8db2bf402
e28166 <e28166@FXH-0015840124>
2025-03-25 08:59:40 -0400

    s

---
+++ src/lessons/WhileLoops.java
@@ -0,0 +1,18 @@
+package lessons;
+
+public class WhileLoops {
+
+	public static void main(String[] args) {
+
+		// while-loops used when you don't know
+		// how many times to repeat a block of code
+		
+		int counter = 0;
+		
+		while(counter < 3) {
+			
+			System.out.println("Hello World!");
+			counter++;
+		}
+	}
+}
