e24ffdc3c6ec2e24c36a04dab20f7efeeb7ae215
e28166 <e28166@FXH-0015840124>
2025-04-03 13:50:34 -0400

    s

---
+++ src/lessons/Modulus.java
@@ -0,0 +1,12 @@
+package lessons;
+
+public class Modulus {
+
+	public static void main(String[] args) {
+	
+		// Returns remainder of a division.
+		// Returns numerator of a proper fraction.
+		System.out.println(7%88);	// 7
+	}
+
+}
