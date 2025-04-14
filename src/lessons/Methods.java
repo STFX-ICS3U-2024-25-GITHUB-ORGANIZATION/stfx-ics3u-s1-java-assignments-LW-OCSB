d36766be1f469a022257406d6de5637d9c25f598
e28166 <e28166@FXH-0015840124>
2025-04-14 09:17:00 -0400

    s

---
+++ src/lessons/Methods.java
@@ -0,0 +1,68 @@
+package lessons;
+
+public class Methods {
+	
+	public static void main(String[] args) {
+		
+		System.out.println(biggerOfTwoIntegers(3.0, 4));
+		printSomething();
+		
+	}
+	
+	
+	// NOTE - The 3 lines below are called method "header",
+	//	      which are comments which defines the method.
+	
+	// DESCRIPTION - Returns greater of two integers.
+	// PARAMETERS - int a, int b
+	// RETURN - int
+	public static int biggerOfTwoIntegers(int a, int b)  {   // this entire line is called the "method signature"
+		
+		System.out.println("First method used.");
+		
+		if (a>b)
+			return a;
+		
+		else 
+			return b;
+	}
+
+	
+	// NOTE - The method below is "overloaded" since it has the exact
+	//		  same method name, but different return type and/or parameters.
+	//		  In other words, the method signatures are unique.
+	
+	// DESCRIPTION - Returns greater of two integers.
+	// PARAMETERS - int a, int b
+	// RETURN - int
+	public static double biggerOfTwoIntegers(double a, double b)  {
+		
+		System.out.println("Second method used.");
+		
+		if (a>b)
+			return a;
+		
+		else 
+			return b;
+	}
+	
+	
+	// NOTE - void methods do not return anything.  You can perform
+	// 		  any task inside a void method, but he keyword "return" cannot 
+	//	      exist in a void method.  void methods are useful for printing.
+	
+	// DESCRIPTION - Prints a String.
+	// PARAMETERS - na
+	// RETURN - void 
+	public static void printSomething()  {
+		
+		System.out.println("Hello World!");
+	}
+	
+}
+
+
+
+
+
+
